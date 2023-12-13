9292ccbd04fd4fdb2dbe8190e74f555f64572a6d
allamymp <allamympereira@gmail.com>
2023-12-13 10:30:47 -0300

    update

--- src/main/java/br/edu/ifpe/recife/gcms/webapp/HomeController.java
+++ src/main/java/br/edu/ifpe/recife/gcms/webapp/HomeController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
     @RequestMapping("/")
     public @ResponseBody String greeting() {
-        return "Hello, World";
+        return "Helloo, World";
     }
 
     @RequestMapping("/allamy")
     public @ResponseBody String greetingFulano() {
-        return "Hello, Allamy!";
+        return "Helloo, Allamy!";
     }
 }
