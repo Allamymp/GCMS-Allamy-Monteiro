b22dcdea03b8cf668be34b1c36dca80c3f60e0dd
Allamy Monteiro <Allamympereira@gmail.com>
2023-12-20 10:37:06 -0300

    Update Pratica2Application.java

--- src/main/java/br/edu/ifpe/recife/gcms/webapp/Pratica2Application.java
+++ src/main/java/br/edu/ifpe/recife/gcms/webapp/Pratica2Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Pratica2Application {
 
 	public static void main(String[] args) {
+
+		BuildBraker build = new BuildBreaker(10);
 		SpringApplication.run(Pratica2Application.class, args);
 	}
 
